0eb6f86dfc381e1d173acdf37d77d7d7bc1554f2
Marina McPeak <mcpeakmarina@gmail.com>
2023-11-02 23:38:33 -0600

    added enum for swipe directions

---
+++ PaletteSwipe/SwipeDirection.swift
@@ -0,0 +1,14 @@
+//
+//  SqipeDirection.swift
+//  PaletteSwipe
+//
+//  Created by Marina McPeak on 2023-11-02.
+//
+
+/// An enumeration representing the possible swipe directions in a gesture.
+enum SwipeDirection {
+    case up    /// Represents a swipe upward.
+    case down  /// Represents a swipe downward.
+    case left  /// Represents a swipe leftward.
+    case right /// Represents a swipe rightward.
+}
